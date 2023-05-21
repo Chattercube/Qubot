2e75a2bfa5841a875d2a2d13eec407bc1a342e14
Chattercube <happyeenoddy@gmail.com>
2023-05-21 22:52:24 +0800

    123

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+const PREFIX = "?"
+
+async function process_command(message){
+
+    if(!message.content.startsWith(PREFIX) || message.authot.bot ) return;
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+
+    const messageArray = message.content.split("");
+    const argument = messageArray.slice(1);
+    const cmd = messageArray[0];
+
+if (command == 'ping'){
+    msg = message.channel.send("Bot is working!");
+    msg.react('🤔');
+}
+}
+
+
+
 // Requirements and Variables
-import {process_command} from "./cmd.js";
+
+
 require('dotenv').config();
 const keepAlive = require(`./server`);
 const { Client } = require('discord.js');
