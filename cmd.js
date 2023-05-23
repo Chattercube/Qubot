b1c1bb24140c9fb416b9dc9802c8a85441a48a45
Chattercube <33176933+Chattercube@users.noreply.github.com>
2023-05-23 11:04:31 +0000

    Big update

--- cmd.js
+++ cmd.js
@@ -1,19 +1,47 @@
-const PREFIX = "?"
+const PREFIX = "?";
+const MOD_ROLENAME = "Qmod";
 
-export async function process_command(message){
+async function process_command(message) {
 
-    if(!message.content.startsWith(PREFIX) || message.authot.bot ) return;
-    const args = message.content.slice(PREFIX.length).split(/ +/);
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
-    const messageArray = message.content.split("");
-    const argument = messageArray.slice(1);
-    const cmd = messageArray[0];
+  console.log("Received Msg")
 
-if (command == 'ping'){
-    msg = message.channel.send("Bot is working!");
-    msg.react('🤔');
+  const args = message.content.slice(PREFIX.length).split(/ +/);
+  console.log(args)
+
+  if (args[0] == 'ping') {
+    msg = await message.channel.send("pong");
+    msg.react("🤔")
+  }
+
+  if (args[0] == "mod" && args.length == 1){
+    if(message.member.roles.cache.some(r => r.name == MOD_ROLENAME)){
+      msg = message.channel.send("You are a moderator");
+    } else{
+      msg = message.channel.send("You are not in the sudoers file");
+    }
+  }
+
+  if (args[0] == "man" && args.length == 1){
+    const fs = require('fs');
+    let rawdata = fs.readFileSync('manual.json')
+    let manual = JSON.parse(rawdata);
+    let embed_description = "";
+    for (manual_entry in manual){
+      console.log(`${manual_entry} : ${manual[manual_entry].usage}`);
+      embed_description += `\`${manual_entry}\` : ${manual[manual_entry].usage}\n`;
+    };
+
+    const embed_msg = {
+      color : 0x0b8edf,
+      title : "Instructions",
+      description : embed_description
+    };
+
+    message.author.send({embeds: [embed_msg]});
+    
+  }
 }
 
-
-
-}
+module.exports = process_command;
--- index.js
+++ index.js
@@ -1,40 +1,29 @@
-const PREFIX = "?"
 
-async function process_command(message){
 
-    if(!message.content.startsWith(PREFIX) || message.authot.bot ) return;
-    const args = message.content.slice(PREFIX.length).split(/ +/);
 
-    const messageArray = message.content.split("");
-    const argument = messageArray.slice(1);
-    const cmd = messageArray[0];
-
-if (command == 'ping'){
-    msg = message.channel.send("Bot is working!");
-    msg.react('🤔');
-}
-}
 
 
 
 // Requirements and Variables
 
 
+
+const process_command = require('./cmd');
 require('dotenv').config();
 const keepAlive = require(`./server`);
 const { Client } = require('discord.js');
-const client = new Client({ intents: 65535 });
-
-
+const client = new Client({ intents: 3276799 });
 // Array of Command objects
 client.on('messageCreate', process_command);
 
+client.on('error', (err) => {
+  console.log(err.message)
+});
+
 // Ready Event
 client.on('ready', async () => {
-    console.log(`Testing bot is now online successfully!`);
-    await client.guilds.cache
-            .get(process.env['serverId'])
-            .commands.set(cmds);
+  console.log(`Testing bot is now online successfully!`);
+  await client.guilds.cache.get(process.env['serverId'])
 });
 
 // Bot Login
